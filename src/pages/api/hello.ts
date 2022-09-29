80632a94c430fa72d5b5da5a09d29aabd3acab05
tanaliga100 <jordantanaliga@gmail.com>
2022-09-29 20:57:28 +0800

    modified-api-ts

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -7,7 +7,7 @@ type Data = {
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>D
+  res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: "John oe" });
+  res.status(200).json({ name: "John Doe" });
 }
