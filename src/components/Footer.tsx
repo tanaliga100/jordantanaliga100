69aa5d024c8d9f66956fa879402e163cfa98a068
tanaliga100 <jordantanaliga@gmail.com>
2022-12-15 23:10:11 +0800

    adjust-footer-padding

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,9 +7,9 @@ const Footer = (props: Props) => {
   return (
     <div
       id="footer"
-      className="font-thin text-white  md:p-10 w-full  bg-slate-900 text-center  items-center"
+      className="font-thin text-white p-6 md:p-10 w-full  bg-slate-900 text-center  items-center"
     >
-      <div className="my-3  text-xs">
+      <div className="my-3 text-xs">
         <p className="font-thin text-xs md:text-2xl text-white/60 tracking-widest px-2 animate-pulse ">
           Whatever I do, I do it enthusiastically... so people will love it more
         </p>
